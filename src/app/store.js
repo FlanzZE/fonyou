a0a58f3e4ce7a480782d6b5ac8689e5834e85969
Victor Mexicano <vmmexicano@gmail.com>
2021-10-14 13:35:00 -0500

    Updates

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import charactersReducer from './reducer';
-const store = configureStore({
-  reducer: {
-    charactersData: charactersReducer
-  },
-});
-export default store
--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Characters } from './Characters'
-export { default as Character } from './Character'
-export { default as ModalComponent } from './Modal'
-export { default as Pagination } from './Pagination'
